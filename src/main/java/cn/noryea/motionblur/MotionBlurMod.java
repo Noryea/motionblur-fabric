e0c2efaa4f93cd0dca0a3158b39830c1978efef2
Noryea <864460295@qq.com>
2022-11-20 09:41:40 +0800

    Usa satin so this is compatible with sodium.

--- src/main/java/cn/noryea/motionblur/MotionBlurMod.java
+++ src/main/java/cn/noryea/motionblur/MotionBlurMod.java
@@ -1,32 +1,39 @@
 package cn.noryea.motionblur;
 
 import cn.noryea.motionblur.config.MotionBlurConfig;
+import com.google.gson.JsonSyntaxException;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import ladysnake.satin.api.event.ShaderEffectRenderCallback;
+import ladysnake.satin.api.managed.ManagedShaderEffect;
+import ladysnake.satin.api.managed.ShaderEffectManager;
+import ladysnake.satin.api.managed.uniform.Uniform1f;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.ShaderEffect;
 import net.minecraft.resource.Resource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class MotionBlurMod implements ClientModInitializer {
 
-    public static Logger LOGGER = LoggerFactory.getLogger("MotionBlurMod");
+    public static String ID = "motionblur";
+    private float currentBlur;
 
-    private static final MotionBlur motionBlur = new MotionBlur();
-
-    public static HashMap<Identifier, Resource> runtimeResources = new HashMap<>();
+    private final ManagedShaderEffect motionblur = ShaderEffectManager.getInstance().manage(new Identifier(ID, "shaders/post/motion_blur.json"),
+            shader -> shader.setUniformValue("BlendFactor", getBlur()));
 
     @Override
     public void onInitializeClient() {
         MotionBlurConfig.init("motionblur", MotionBlurConfig.class);
-        getCurrentMotionBlur().init();
 
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
   		    dispatcher.register(
@@ -37,6 +44,16 @@ public class MotionBlurMod implements ClientModInitializer {
                     .then(ClientCommandManager.literal("toggle")
                             .executes(context -> toggle(context.getSource())))
             );
+        });
+
+        ShaderEffectRenderCallback.EVENT.register((deltaTick) -> {
+            if (MotionBlurConfig.enable) {
+                if(currentBlur!=getBlur()){
+                    motionblur.setUniformValue("BlendFactor", getBlur());
+                    currentBlur=getBlur();
+                }
+                motionblur.render(deltaTick);
+            }
         });
     }
 
@@ -59,7 +76,8 @@ public class MotionBlurMod implements ClientModInitializer {
         return 1;
     }
 
-    public static MotionBlur getCurrentMotionBlur() {
-        return motionBlur;
+    public float getBlur() {
+        return Math.min(MotionBlurConfig.motionBlurAmount, 99)/100F;
     }
+
 }
